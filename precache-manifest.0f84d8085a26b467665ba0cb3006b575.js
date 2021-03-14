25a3d0a5e0992c6a1940569c8e9e3f2db8e7ecbe
Egor Stronhin <i@egor.xyz>
2021-03-14 20:10:23 +0200

    Updates

---
+++ precache-manifest.0f84d8085a26b467665ba0cb3006b575.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35924e6a648ee2a8bc1cfb2427234ba7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a854b63f32f10e41205d",
+    "url": "/static/css/2.94da2a06.chunk.css"
+  },
+  {
+    "revision": "d61fcba0bf2d22edfa2d",
+    "url": "/static/css/main.78e6b959.chunk.css"
+  },
+  {
+    "revision": "a854b63f32f10e41205d",
+    "url": "/static/js/2.5ff147b7.chunk.js"
+  },
+  {
+    "revision": "e18637604789af6a40503615de5bd745",
+    "url": "/static/js/2.5ff147b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d61fcba0bf2d22edfa2d",
+    "url": "/static/js/main.249e8727.chunk.js"
+  },
+  {
+    "revision": "077f45d6d3f70d48ca5e",
+    "url": "/static/js/runtime-main.d8cff8f8.js"
+  },
+  {
+    "revision": "066a8cb0c420f2462d2d6214d3ad426a",
+    "url": "/static/media/devKitty.066a8cb0.svg"
+  }
+]);
